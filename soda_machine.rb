6137b70a673b5701a35457fa88cea829898320f3
Jason Sutter <jasonsutter87@gmail.com>
2016-05-09 15:00:33 -0700

    still needs work to pass some tests

--- soda_machine.rb
+++ soda_machine.rb
@@ -7,12 +7,23 @@ class SodaMachine
   end
 
   def current_inventory_count
+    soda_count = []
+    sodas.each do |x|
+       soda_count << x
+    end
+    return soda_count.length
   end
 
   def find_soda(soda_brand)
+    if sodas.index(soda_brand) != nil
+      return sodas[0]
+    else
+      nil
+    end
   end
 
   def sell(soda_brand)
+
   end
 
 end
